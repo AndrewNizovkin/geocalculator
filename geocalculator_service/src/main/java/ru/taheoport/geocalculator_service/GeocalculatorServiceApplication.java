d4c04fa33dd3b3b2d99137b3e18e36b4ae31602c
andrew-desktop <andreynizovkin@inbox.ru>
2024-08-07 15:40:12 +0300

    Feat: created geocalculator_service

---
+++ geocalculator_service/src/main/java/ru/taheoport/geocalculator_service/GeocalculatorServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.taheoport.geocalculator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocalculatorServiceApplication.class, args);
+		System.out.println("Hello, World");
+	}
+
+}
---
+++ geocalculator_service/src/test/java/ru/taheoport/geocalculator_service/GeocalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.taheoport.geocalculator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
