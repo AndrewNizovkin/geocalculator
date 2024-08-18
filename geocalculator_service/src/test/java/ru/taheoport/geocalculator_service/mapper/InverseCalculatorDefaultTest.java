a39ddd7a58b6c3a436e8a2eb16d558595a2aadef
andrew-desktop <andreynizovkin@inbox.ru>
2024-08-18 15:33:28 +0300

    Work: inverseTaskService without database

--- geocalculator_service/src/test/java/ru/taheoport/geocalculator_service/mapper/InverseCalculatorDefaultTest.java
+++ geocalculator_service/src/test/java/ru/taheoport/geocalculator_service/mapper/InverseCalculatorDefaultTest.java
@@ -1,11 +1,9 @@
 package ru.taheoport.geocalculator_service.mapper;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.taheoport.geocalculator_service.model.InverseTask;
 
 import static org.junit.jupiter.api.Assertions.*;
 
