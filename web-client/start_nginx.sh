899a884424ce72a8bdcff01dabb911fb73bc8e94
andrew-desktop <andreynizovkin@inbox.ru>
2024-11-06 16:33:51 +0300

    Fix: updates start_nginx.sh

--- web-client/start_nginx.sh
+++ web-client/start_nginx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --restart always --name nginx1 -p 8080:80 -v /home/andrew/MyProjects:/usr/share/nginx/html nginx
+docker run -d --restart always --name nginx1 -p 8080:80 -v /home/andrew/MyJava/geocalculator/web-client:/usr/share/nginx/html nginx
